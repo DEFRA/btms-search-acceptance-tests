b9c47878ad13ceb236e2b15a84b6fff772738998
Ade Oke <adesmail100@gmail.com>
2025-01-31 12:25:07 +0000

    * all locators for the search results page for a single declaration and no ched

--- test/page-objects/search_results.page.js
+++ test/page-objects/search_results.page.js
@@ -5,6 +5,42 @@ class SearchResultsPage extends Page {
   get heading() {
     return $('h1')
   }
+
+  get mrn() {
+    return $('#main-content > div > div > dl > div:nth-child(1) > dd')
+  }
+
+  get status() {
+    return $('#main-content > div > div > dl > div:nth-child(2) > dd')
+  }
+
+  get tableItemNumber(){
+    return $('#main-content > div > div > table > tbody > tr > th')
+  }
+
+  get commodityCode(){
+    return $('#main-content > div > div > table > tbody > tr > td:nth-child(2)')
+  }
+
+  get description(){
+    return $('#main-content > div > div > table > tbody > tr > td:nth-child(3)')
+  }
+
+  get quantity(){
+    return $('#main-content > div > div > table > tbody > tr > td:nth-child(4)')
+  }
+
+  get chedReference(){
+    return $('#main-content > div > div > table > tbody > tr > td:nth-child(5)')
+  }
+
+  get matchStatus(){
+    return $('#main-content > div > div > table > tbody > tr > td:nth-child(6)')
+  }
+
+  get decision(){
+    return $('#main-content > div > div > table > tbody > tr > td:nth-child(7)')
+  }
 }
 
 export default new SearchResultsPage()
