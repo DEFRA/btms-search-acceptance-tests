b7225355bad25f30e073139e1f4868fe1de6abeb
Ade Oke <adesmail100@gmail.com>
2025-02-13 09:41:55 +0000

    final attempt at enabling visibility to deplyed services from github

--- test/specs/example.e2e.js
+++
@@ -1,7 +0,0 @@
-import { browser } from '@wdio/globals'
-
-describe('Search page', () => {
-  it('Should be on the search page', async () => {
-    await browser.url('/search');
-  })
-})
--- test/specs/search.e2e.js
+++ test/specs/search.e2e.js
@@ -1,25 +1,25 @@
-// import { browser, expect } from '@wdio/globals'
-// import SearchPage from '../page-objects/search.page'
-// import SearchResultsPage from '../page-objects/search_results.page'
-// import noMatch from '../../data/movement/nomatch.json'
-//
-// describe('Search page', () => {
-//   it('Should be on the search page', async () => {
-//     await SearchPage.open()
-//
-//     await expect(browser).toHaveTitle('Search - Border Trade Matching Service')
-//
-//     await expect(SearchPage.searchTextBox).toBeDisplayed()
-//     await expect(SearchPage.searchButton).toBeDisplayed()
-//   })
-//
-//   it('Should navigate to the search results page for a valid MRN search', async () => {
-//     await SearchPage.open()
-//     await SearchPage.searchFor(noMatch._id)
-//
-//     await expect(browser).toHaveTitle(
-//       'Search result - Border Trade Matching Service'
-//     )
-//     await expect(SearchResultsPage.heading).toBeDisplayed()
-//   })
-// })
+import { browser, expect } from '@wdio/globals'
+import SearchPage from '../page-objects/search.page'
+import SearchResultsPage from '../page-objects/search_results.page'
+import noMatch from '../../data/movement/nomatch.json'
+
+describe('Search page', () => {
+  it('Should be on the search page', async () => {
+    await SearchPage.open()
+
+    await expect(browser).toHaveTitle('Search - Border Trade Matching Service')
+
+    await expect(SearchPage.searchTextBox).toBeDisplayed()
+    await expect(SearchPage.searchButton).toBeDisplayed()
+  })
+
+  it('Should navigate to the search results page for a valid MRN search', async () => {
+    await SearchPage.open()
+    await SearchPage.searchFor(noMatch._id)
+
+    await expect(browser).toHaveTitle(
+      'Search result - Border Trade Matching Service'
+    )
+    await expect(SearchResultsPage.heading).toBeDisplayed()
+  })
+})
