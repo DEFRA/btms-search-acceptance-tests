f5b720002effb9c64c1fca25f65b5a2da0646c91
Ade Oke <adesmail100@gmail.com>
2025-02-19 23:46:57 +0000

    changed the port to 3000

--- wdio.github.conf.js
+++ wdio.github.conf.js
@@ -7,7 +7,7 @@ const oneMinute = 60 * 1000;
 let baseUrl;
 
 if (process.env.ENVIRONMENT === "local") {
-  baseUrl = "http://localhost:8080";
+  baseUrl = "http://localhost:3000";
 } else if (process.env.ENVIRONMENT === "dev") {
   baseUrl = "https://btms-portal-frontend.dev.cdp-int.defra.cloud";
 } else if (process.env.ENVIRONMENT === "test") {
