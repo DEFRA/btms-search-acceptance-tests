01d0bf68110113e8bc845ad60df17cb51ca70241
Ade Oke <adesmail100@gmail.com>
2025-02-26 13:17:45 +0000

    re-adding asslure reports

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 echo "run_id: $RUN_ID"
+npm test
 
-npx wdio run ./wdio.conf.js
+npm run report:publish
+publish_exit_code=$?
 
-./bin/publish-tests.sh
+if [ $publish_exit_code -ne 0 ]; then
+  echo "failed to publish test results"
+  exit $publish_exit_code
+fi
 
 # At the end of the test run, if the suite has failed we write a file called 'FAILED'
 if [ -f FAILED ]; then
