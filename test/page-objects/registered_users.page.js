16e0a792b1cdd3d85e61ca2535babe2c4f36e43b
Ade Oke <adesmail100@gmail.com>
2025-03-12 09:41:02 +0000

    updates to the registration page for environments that require authentication in order to perform a search

--- test/page-objects/registered_users.page.js
+++ test/page-objects/registered_users.page.js
@@ -3,14 +3,13 @@ import { browser, $$ } from "@wdio/globals";
 import { analyse } from "../../dist/wcagchecker.cjs";
 
 class RegisteredUsersPage extends Page {
-  get userLogins() {
-    return $$('.govuk-table__body .govuk-table__row .govuk-table__cell:first-of-type a')
+  userLogins(input) {
+    return $$(`//th[text()='${input}']/parent::tr/td/a`)
   }
 
-  // FIXME: Temporary while a user is seeded that will be available in all environments
-  async loginAsFirstUser(){
+  async loginAsUser(input){
     await analyse(browser, "");
-    await this.userLogins[0].click();
+    await this.userLogins(input)[0].click();
   }
 }
 
--- test/specs/home.e2e.js
+++ test/specs/home.e2e.js
@@ -4,15 +4,15 @@ import RegisteredUsersPage from "page-objects/registered_users.page.js";
 import SearchPage from "page-objects/search.page.js";
 
 describe("Home page", () => {
-  // it("Should have a login button", async () => {
-  //   await Home.open();
-  //   await expect(Home.loginBtn).toBeDisplayed();
-  // });
+  it("Should have a login button", async () => {
+    await Home.open();
+    await expect(Home.loginBtn).toBeDisplayed();
+  });
 
   it("Should login a registered user and navigate to the search page", async () => {
     await Home.open();
     await Home.login();
-    await RegisteredUsersPage.loginAsFirstUser();
+    await RegisteredUsersPage.loginAsUser(process.env.USR);
     await expect(await SearchPage.isSearchTextBoxDisplayed()).toBe(true);
   });
 });
